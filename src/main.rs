f6fbdcf4753d38e5a3dd66e5f041e83321100913
FrK5E <FrK5Ewzb4XbBN1aWlGUZ@proton.me>
2023-03-13 22:04:36 +0100

    revisit the approach which was flawed. This new approach may also be flawed, but we'll see.

--- src/main.rs
+++ src/main.rs
@@ -38,10 +38,6 @@ fn get_digits_hash(k: u64) -> u64 {
     defhasher.finish()
 }
 
-fn number_of_digits(k: u64) -> u32 {
-    let a = (k as f64).log10().floor();
-    (a as u32) + 1
-}
 
 fn pow10(e: u32) -> u64 {
     let mut result: u64 = 1;
@@ -53,21 +49,24 @@ fn pow10(e: u32) -> u64 {
     return result;
 }
 
-fn get_next_lower_common_multiple(k: u64) -> u64 {
-    let mut res = k;
-    let mut repeat_flag = true;
-    while repeat_flag {
-        repeat_flag = false;
-        for i in 2..7 {
-            if res % i != 0 {
-                res = res - 1;
-                repeat_flag = true;
-                break;
-            }
-        }
-        
+fn starts_with_one( i:u64 ) -> bool { 
+
+    while i > 10 { 
+        i = i/10;
     }
-    return res;
+    i<2
+}
+
+fn ends_with_one_or_zero(i:u64) -> bool {
+
+    let ld = i % 10;
+
+    ld==0 || ld==5  
+
+}
+
+fn contains_at_least_one_even_digit(i:u64) -> bool { 
+    true
 }
 
 fn main() {
@@ -77,36 +76,58 @@ fn main() {
     println!("hash 2 {}", get_digits_hash(124));
     assert!(get_digits_hash(123) == get_digits_hash(321));
     assert!(get_digits_hash(112323) == get_digits_hash(332211));
-    assert!(number_of_digits(112323) == number_of_digits(332211));
     assert!(pow10(1) == 10);
     assert!(pow10(3) == 1000);
-    assert!(get_next_lower_common_multiple(100) == 60);
+    assert!(starts_with_one(12) == true );
+    assert!(starts_with_one(21) == false );
+    assert!(ends_with_one_or_zero(0)==true);
+    assert!(ends_with_one_or_zero(132738273827832)==false);
+    assert!(ends_with_one_or_zero(132738273827835)==true);
+    assert!(ends_with_one_or_zero(132738273827830)==true);
 
-    let lcm: u64 = 60;
+
     let mut power_of_ten = 3;
     let mut found: bool = true;
     let mut i: u64;
 
-    loop {
-        i = pow10(power_of_ten);
-        i = get_next_lower_common_multiple(i);
+    // x, 2x, 3x, 4, 5x, 6x are written by same digits
+    // therefore: 
+    // x starts with 1, because otherwise 6x has more digits
+    // x ends with 1 or 0: because 5x ends with 0 or five
+    // x is divible by 3: because 3x is divisible by three and that is equivalent to sum of digits is divisible by three
+    // x contains at least one of { 0,2,4,6,8 }: because 2x is even and even number ends with even digit.
 
-        loop {
-            let h = get_digits_hash(i);
-            let nd_h = number_of_digits(i);
+
+    loop {
+        i = pow10(power_of_ten)+2; // plus 2 so that sum of digits is three and therefore so that the number is divisible by three 
+
+        assert!( i % 3 == 0 );
+
+        loop { 
+             
             found = true;
-            let mut next_power_of_ten: bool = false;
+
+            if !starts_with_one(i) {
+                power_of_ten += 1;  
+                found = false;
+                break;
+            }
+
+            if !ends_with_one_or_zero(i) { 
+                found = false;
+                break;
+            }
+
+            if !contains_at_least_one_even_digit(i) { 
+                found = false;
+                break; 
+            }
+
+            let h = get_digits_hash(i);
+
 
             for k in (2..7).rev() {
-                let i2 = i / k;
-                let nd_h2 = number_of_digits(i2);
-
-                if nd_h2 < nd_h {
-                    // here we need to jump to next power of ten
-                    found = false;
-                    next_power_of_ten = true;
-                    break;
-                }
+                let i2 = i * k;
 
                 let h2 = get_digits_hash(i2);
 
@@ -116,14 +137,11 @@ fn main() {
                 }
             }
 
-            if next_power_of_ten {
-                power_of_ten += 1;
-                break;
-            }
+            
             if found { 
                 break;
             }
-            i -= lcm;
+            i += 3;
         }
         if found { 
             break;
